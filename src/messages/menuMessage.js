1e133a812f48e5ad018d159f3d68d2844ba6d4e3
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2023-10-01 06:59:31 +0000

    Update menuMessage.js and prefrencesMessage.js
    Update in Messages According to the UI changes

--- src/messages/menuMessage.js
+++ src/messages/menuMessage.js
@@ -1,24 +1,27 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require("discord.js");
 
 const menuMessage = new EmbedBuilder()
-    .setThumbnail('https://res.cloudinary.com/dn2jk5smj/image/upload/v1694522904/purrfit/menuIcon_znqeyf.png')
-    .addFields(
-        {
-            name: 'Meow! Getting Started with Purrfit',
-            value: 'Hey there! I\'m Purrfit, the purr-fect pal for gamers who care about their health. I\'ll nudge you kindly, keep track of your activities, and help you juggle fun and fitness. Pawsome, right?'
-        },
-        {
-            name: 'How to Begin',
-            value: `
+  .setThumbnail(
+    "https://res.cloudinary.com/dn2jk5smj/image/upload/v1694522904/purrfit/menuIcon_znqeyf.png",
+  )
+  .addFields(
+    {
+      name: "Meow! Getting Started with Purrfit",
+      value:
+        "Hey there! I'm Purrfit, the purr-fect pal for gamers who care about their health. I'll nudge you kindly, keep track of your activities, and help you juggle fun and fitness. Pawsome, right?",
+    },
+    {
+      name: "How to Begin",
+      value: `
             1. Meow! Just type **/preferences** to pick your health areas and how often you want reminders during playtime. Once your goals are set, you can view them by typing **/goal**.\n2. Start your game with **/start**, end with **/end**, and take a break with **/pause**.\n3. Peek at your progress with **/profile @yourname** to view your goals, ranking, and your activity history. Purrfect! 🐾
-            `
-        },
-        {
-            name: 'Other Helpful Commands',
-            value: `
-            - **/menu** - To learn how to get started, view commands, and set preferences.\n- **/commands** - To view all commands.\n- **/profile @username** - To view profile, including goals, ranks, and activity history.\n- **/mute** - Temporarily disables all notifications and messages.
-            `
-        }
-    )
+            `,
+    },
+    {
+      name: "Other Helpful Commands",
+      value: `
+            - **/menu** - To learn how to get started, view commands, and set preferences.\n- **/commands** - To view all commands.\n- **/profile** - To view profile, including goals, ranks, and activity history.\n- **/mute** - Temporarily disables all notifications and messages.
+            `,
+    },
+  );
 
 module.exports = menuMessage;
--- src/messages/prefrencesMessage.js
+++ src/messages/prefrencesMessage.js
@@ -1,65 +1,68 @@
-const { EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require('discord.js');
-const goals = require('../data/goals');
-const frequencies = require('../data/frequencies');
-
-
-
-
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  StringSelectMenuBuilder,
+  StringSelectMenuOptionBuilder,
+} = require("discord.js");
+const goals = require("../data/goals");
+const frequencies = require("../data/frequencies");
 
 const preferencesEmbed = new EmbedBuilder()
-    .setThumbnail('https://res.cloudinary.com/dn2jk5smj/image/upload/v1694522904/purrfit/preferecesIcon_rnw95u.png')
-    .setFields(
-        {
-            name: '🎯 Setting Goals and Reminders',
-            value: `"Meow! Pick your goals like sitting right, drinking water, or resting your eyes, then choose how often, and we'll help you game comfy and happy!" 🐱`
-        },
-        {
-            name: `🐱 To set reminders, follow these steps using the drop down menus below:`,
-            value: `
+  .setThumbnail(
+    "https://res.cloudinary.com/dn2jk5smj/image/upload/v1694522904/purrfit/preferecesIcon_rnw95u.png",
+  )
+  .setFields(
+    {
+      name: "🎯 Setting Goals and Reminders",
+      value: `"Meow! Pick your goals like sitting right, drinking water, or resting your eyes, then choose how often, and we'll help you game comfy and happy!" 🐱`,
+    },
+    {
+      name: `🐱 To set reminders, follow these steps using the drop down menus below:`,
+      value: `
             1. Select any focus areas from the drop down menu below. (You can select as many as you want!)
 
-            2. Select the frequency for the reminders from the drop down menu below.`
-        },
-    )
+            2. Select the frequency for the reminders from the drop down menu below.`,
+    },
+  );
 
 const goalSelectMenu = new StringSelectMenuBuilder()
-    .setCustomId('goalPreferences')
-    .setPlaceholder('Select your health area')
-    .addOptions(
-        goals.map((goal) =>
-            new StringSelectMenuOptionBuilder()
-                .setLabel(goal.label)
-                .setValue(goal.value)
-                .setDescription(goal.description)
-        )
-    )
-    .setMinValues(1)
-    .setMaxValues(5)
+  .setCustomId("goalPreferences")
+  .setPlaceholder("Select your health area")
+  .addOptions(
+    goals.map((goal) =>
+      new StringSelectMenuOptionBuilder()
+        .setLabel(goal.label)
+        .setValue(goal.value)
+        .setDescription(goal.description),
+    ),
+  )
+  .setMinValues(1)
+  .setMaxValues(6);
 
 const frequencySelectMenu = new StringSelectMenuBuilder()
-    .setCustomId('frequencyPreferences')
-    .setPlaceholder('Select your frequency')
-    .addOptions(
-        frequencies.map((frequency) =>
-            new StringSelectMenuOptionBuilder()
-                .setLabel(frequency.label)
-                .setValue(frequency.value)
-                .setDescription(frequency.description)
-        )
-    )
-    .setMinValues(1)
-    .setMaxValues(1)
+  .setCustomId("frequencyPreferences")
+  .setPlaceholder("Select your frequency")
+  .addOptions(
+    frequencies.map((frequency) =>
+      new StringSelectMenuOptionBuilder()
+        .setLabel(frequency.label)
+        .setValue(frequency.value)
+        .setDescription(frequency.description),
+    ),
+  )
+  .setMinValues(1)
+  .setMaxValues(1);
 
-const goalActionRow = new ActionRowBuilder()
-    .addComponents(goalSelectMenu)
+const goalActionRow = new ActionRowBuilder().addComponents(goalSelectMenu);
 
-const frequencyActionRow = new ActionRowBuilder()
-    .addComponents(frequencySelectMenu)
+const frequencyActionRow = new ActionRowBuilder().addComponents(
+  frequencySelectMenu,
+);
 
 const preferencesMessage = {
-    embeds: [preferencesEmbed],
-    components: [goalActionRow, frequencyActionRow],
-    ephemeral: true
-}
+  embeds: [preferencesEmbed],
+  components: [goalActionRow, frequencyActionRow],
+  ephemeral: true,
+};
 
 module.exports = preferencesMessage;
